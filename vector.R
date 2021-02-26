84e9093a4102cc4332b0d098aca36f202a4cc5e1
JAVIER Q.TENORIO <javiemre@gmail.com>
2021-02-25 22:50:03 -0500

    Initial commit of vector.R

--- vector.R
+++ vector.R
@@ -10,3 +10,15 @@ poker_vector
 # Assign days as names of roulette_vector
 names(roulette_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday") 
 roulette_vector
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+poker_vector
+names(roulette_vector) <- days_vector
+roulette_vector
+# Assign to total_daily how much you won/lost on each day
+total_daily <- poker_vector + roulette_vector
+total_daily
